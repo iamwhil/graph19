61b8f82cab85c2d8d3e8411b6e12aead1964f34e
Patrick MacMurchy <pmacmurchy@pairin.com>
2019-03-29 11:33:25 -0600

    Remove comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,6 @@
 
 class Post < ApplicationRecord
-  # include Users::Concerns::Post # don't do this, use the ConcernDirectory
-  # ::ConcernDirectory.inclusions(self).each{ |ext| include ext }
-
   validates_presence_of :title
-
   has_many :comments
 
 end
