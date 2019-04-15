efc8b50d10cc8a2db8a2ecff37bdecff82a616f6
Whil Piavis <ncsupiratecaptain@gmail.com>
2019-04-15 09:40:17 -0600

    removes a whitespace.

--- components/blog/lib/blog/engine.rb
+++ components/blog/lib/blog/engine.rb
@@ -4,7 +4,6 @@ module Blog
 
     # allows migrations to be accessible at root
     initializer :append_migrations do |app|
-
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
